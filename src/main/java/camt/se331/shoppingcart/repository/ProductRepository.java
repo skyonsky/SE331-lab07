37a0eb215b93c612e2cba4f7e3984884fd620ff2
skyonsky <pranpoom_dis@cmu.ac.th>
2016-03-25 00:17:03 +0700

    3.3 added interface ProductRopository, DbProductDao class

---
+++ src/main/java/camt/se331/shoppingcart/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package camt.se331.shoppingcart.repository;
+
+import camt.se331.shoppingcart.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by Dto on 2/7/2015.
+ */
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    }
+
