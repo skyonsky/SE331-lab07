b0f6fa70ddc8f8f84e9e236fd722dfe58146cb2d
skyonsky <pranpoom_dis@cmu.ac.th>
2016-03-28 16:28:30 +0700

    10.5 done

--- src/main/java/camt/se331/shoppingcart/config/DatabaseInitializationBean.java
+++ src/main/java/camt/se331/shoppingcart/config/DatabaseInitializationBean.java
@@ -28,31 +28,50 @@ public class DatabaseInitializationBean implements InitializingBean {
     List<SelectedProduct> selectedProducts = new ArrayList<>();
         @Override
         public void afterPropertiesSet() throws Exception {
-                Product[] initProduct =  {
-                                new Product(1l,"Kindle","the good book reader",6900.00),
-                                new Product(2l,"Surface Pro","The unknow computer",34000.00),
-                                new Product(3l,"Mac pro"," Mac book interim",44000.00),
-                                new Product(4l,"Candle","use for lightenup the world",10.00),
-                                new Product(5l,"Bin","User for what ?",200.00),
-                                new Product(6l,"Telephone", "Call the others",150.00),
-                                new Product(7l,"iPhone","What is it?",26000.00),
-                                new Product(8l,"Galaxy Note 4","Who still use this ?",24000.00),
-                                new Product(9l,"AngularJS","we hate it",2000.00),
-                                new Product(10l,"Mazda 3","Very handsome guy use this",300000.00)
-                                };
+            Product[] initProduct = {
+                    new Product(1l, "Kindle", "the good book reader", 6900.00),
+                    new Product(2l, "Surface Pro", "The unknow computer", 34000.00),
+                    new Product(3l, "Mac pro", " Mac book interim", 44000.00),
+                    new Product(4l, "Candle", "use for lightenup the world", 10.00),
+                    new Product(5l, "Bin", "User for what ?", 200.00),
+                    new Product(6l, "Telephone", "Call the others", 150.00),
+                    new Product(7l, "iPhone", "What is it?", 26000.00),
+                    new Product(8l, "Galaxy Note 4", "Who still use this ?", 24000.00),
+                    new Product(9l, "AngularJS", "we hate it", 2000.00),
+                    new Product(10l, "Mazda 3", "Very handsome guy use this", 300000.00)
 
-                        productRepository.save(Arrays.asList(initProduct));
-                productRepository.save(new Product(1l,"Kindle","the good book reader",6900.00));
+            };
+
+            productRepository.save(Arrays.asList(initProduct));
+            productRepository.save(new Product(1l, "Kindle", "the good book reader", 6900.00));
             SelectedProduct[] initSelectedProduct = {
                     new SelectedProduct(initProduct[2], 5),
                     new SelectedProduct(initProduct[4], 2),
                     new SelectedProduct(initProduct[1], 1),
             };
             selectedProducts.addAll(Arrays.asList(initSelectedProduct));
-            Calendar calendar = new GregorianCalendar(2015,4,7);
+            Calendar calendar = new GregorianCalendar(2015, 4, 7);
             shoppingCart.setSelectedProducts(selectedProducts);
             shoppingCart.setPurchaseDate(calendar.getTime());
             shoppingCart.setId(1L);
             shoppingCartRepository.save(shoppingCart);
-            }
+
+            ShoppingCart shoppingCart2 = new ShoppingCart();
+            SelectedProduct[] initSelectedProduct2 = {
+                    new SelectedProduct(initProduct[2], 2),
+                    new SelectedProduct(initProduct[5], 2),
+                    new SelectedProduct(initProduct[1], 1),
+                    new SelectedProduct(initProduct[9], 1),
+                    new SelectedProduct(initProduct[7], 1)
+            };
+            List<SelectedProduct> selectedProducts2 = new ArrayList<>();
+            selectedProducts2.addAll(Arrays.asList(initSelectedProduct2));
+            Calendar calendar2 = new GregorianCalendar(2015, 4, 7);
+            shoppingCart2.setSelectedProducts(selectedProducts2);
+            shoppingCart2.setPurchaseDate(calendar2.getTime());
+            shoppingCart2.setId(2L);
+            shoppingCartRepository.save(shoppingCart2);
+
+}
+
 }
